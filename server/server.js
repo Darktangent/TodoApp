a6f5e444238377736a5491df276f48a73377f422
Rohan <c1ph3r9@gmail.com>
2018-11-13 12:45:53 -0600

    add patch /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 var express=require('express')
 var bodyParser=require('body-parser')
 var {ObjectID}= require('mongodb')
-var {mongoose}=require('./db/mongoose')
+const {mongoose}=require('./db/mongoose')
 var {Todo}=require('./models/todo')
 var {User}=require('./models/user')
+const _ =require('lodash')
 
 
 var app=express()
@@ -62,6 +63,32 @@ Todo.findOneAndDelete(id).then((todo)=>{
 
 
 
+})
+
+//update
+
+app.patch('/todos/:id',(req,res)=>{
+	var id=req.params.id
+	var body=_.pick(req.body,['text', 'completed'])
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send()
+	}
+
+	if(_.isBoolean(body.completed)&& body.completed){
+		body.completedAt= new Date().getTime();
+	}else {
+		body.completed=false
+		body.completedAt=null
+	}
+	Todo.findByIdAndUpdate(id, {$set:body},{new:true})
+	.then((todo)=>{
+		if(!todo){
+			return res.status(404).send()
+		}
+		res.send({todo})
+	}).catch((e)=>{
+		res.status(400).send()
+	})
 })
 
 
