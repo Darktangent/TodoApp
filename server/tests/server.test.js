5871369189430c1cf2508ab61e747f82490c5456
Rohan <c1ph3r9@gmail.com>
2018-11-13 09:01:15 -0600

    test delete /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -104,3 +104,35 @@ it('should return 404 for non-object id',(done)=>{
 })
 
 })
+
+
+describe('Delete /todos/:id',()=>{
+  it ('should remove a todo with matching id', (done)=>{
+    //which todo to remove
+    var hexId= todos[1]._id.toHexString();
+    request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todo._id).toBe(hexId)
+    })
+    .end((err,res)=>{
+      if(err){
+        return done(err)
+      }
+
+      Todo.findById({hexId}).then((todo)=>{
+        expect(todo).toNotExist()
+        done()
+      }).catch((e)=>done(e))
+    })
+  })
+  it('should return 404 if todo not found',(done)=>{
+  var hexId=new ObjectID().toHexString()
+  request(app)
+  .get(`/todos/${hexId}`)
+  .expect(404)
+  .end(done)
+  })
+  it('should return 404 if object invalid')
+})
